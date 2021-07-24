a21524b9e97551060304ee106a397276711a5659
tdagai <trew3535@gmail.com>
2021-07-23 20:49:56 -0700

    Foundations for the project laid out

---
+++ client/app/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>is it thursday yet?</h1>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const compression = require('compression');
+// const path = require('path');
+
+const app = express();
+const PORT = 1337;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+app.use(compression());
+app.use(express.static('./client/dist/'));
+
+// React router base
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// });
+
+app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`));
